a6ae21c3cf232026012db99b197f936502513414
Anthony Delorie <de.lorie@gmail.com>
2015-09-26 22:44:23 -0400

    what i have so far just got 2 new members

--- views/listenForNightTerror.js
+++ views/listenForNightTerror.js
@@ -3,9 +3,20 @@
  */
 var incomingNightTerror = 'child_added',
     NightTerrors = new Firebase('blinding-inferno-2101.firebaseio.com'),
-    $messages = $('#messages');
+    $messages = $('#messages'),
+//    snd = new Audio("rooster.mp3");
+    snd = new Audio("alarm-clock-01.mp3");
 
 NightTerrors.on(incomingNightTerror, function (possibleNightTerror) {
     var time = new Number(possibleNightTerror.val().timestamp);
     $messages.html('<div>wake up having a night terror</div>');
+    // buffers automatically when created
+    snd.play();
+});
+
+NightTerrors.on(incomingNightTerror, function (possibleNightTerror) {
+    var time = new Number(possibleNightTerror.val().timestamp);
+    $messages.html('<div>wake up having a night terror</div>');
+    // buffers automatically when created
+    snd.play();
 });
